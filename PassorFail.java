9249b257652b1e8af63861ac37a8986aa70d5660
AndrewK <43419144+AndrewKuekam17@users.noreply.github.com>
2020-10-28 12:50:56 -0400

    Create PassorFail.java

---
+++ PassorFail.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+
+/**
+ * Class PassorFail
+ */
+public class PassorFail {
+  public static void main(String[] args) {
+    
+    int classes = 0;
+    int studentPass = 0;
+    
+    while (classes) {
+      try {
+        
+        // test file object
+        File inputFile = new File();
+        FileWriter myWriter = new FileWriter();
+        
+        // let user input
+        Scanner scanner = new Scanner(inputFile);
+        
+        // this reads the next line
+        String line = "";
+        
+        // if 
+        line = scanner.nextLine();
+        
+        // string intoan array
+        String [] lineArray = line.split(" ");
+      }
+    }
+  }
+}
